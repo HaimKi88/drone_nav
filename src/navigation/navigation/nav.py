82afa601f0cfdd8669f3835912e9b992a775c817
HaimKi88 <haimki88@gmail.com>
2025-06-22 17:25:10 +0300

    changes in nav

--- src/navigation/navigation/nav.py
+++ src/navigation/navigation/nav.py
@@ -1,47 +1,35 @@
 import rclpy
 from rclpy.node import Node
-from nav_msgs.msg import Odometry
+from sensor_msgs.msg import NavSatFix
+from geographic_msgs.msg import GeoPoint
  
-class nodeName(Node):
-    '''
-    this node do this and this:
-        subscribers: 
-        publishers: 
-    '''
+class Nav(Node):
     
     def __init__(self):
-        super().__init__('node_name')
-        self.get_logger().info("<node-name> initiated")
-        self._init_members()
-        self._init_subscribers()
+        super().__init__('nav')
+        self.get_logger().info("nav initiated")
         self._init_publishers()
-        self._init_timers()
+        self._init_subscribers()
             
-    def _init_members(self):
-        self.msg = Odometry()
-        self.declare_parameter("some_param", 1.0)
-        self.some_param = self.get_parameter("some_param").value
-
     def _init_subscribers(self):
-        self._odom_sub = self.create_subscription(Odometry, "/odom", self._get, 10)
-        self.get_logger().info("<node-name> subscribers initialized")
+        self._coordinates_sub = self.create_subscription(NavSatFix, "/simple_drone/gps/nav", self.get_drone_coordinates, 10)
+        self._target_sub = self.create_subscription(GeoPoint, "/target_location", self.get_target, 10)
+        self.get_logger().info("nav subscribers initialized")
     
     def _init_publishers(self):
-        self._publisher = self.create_publisher(Odometry, "/odom", 10)
-        self.get_logger().info("<node-name> publishers initialized")
+        self.coordinates_publisher = self.create_publisher(NavSatFix, "/robot_location", 10)
+        self.get_logger().info("nav publishers initialized")
 
-    def _init_timers(self):
-        self._pub_timer = self.create_timer(1/10.0, self._publish_methoed)
-
-    def _get(self,msg):
-        pass
+    def get_drone_coordinates(self, msg):
+        drone_coordinates = msg
+        self.coordinates_publisher.publish(drone_coordinates)
     
-    def _publish_methoed(self):
-        pass
-
+    def get_target(self, msg):
+        geo = GeoPoint()
+        
 def main(args=None):
     rclpy.init(args=args)
-    node = nodeName()
+    node = Nav()
 
     try:
         rclpy.spin(node)
